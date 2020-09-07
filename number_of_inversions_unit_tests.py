5dd4106f220b36e28629df1dcdb2d1bedf6f4281
Kristen (Giang) Phan <hgiang.phan@yahoo.com.vn>
2020-09-07 12:12:51 +0700

    Add files via upload

---
+++ number_of_inversions_unit_tests.py
@@ -0,0 +1,30 @@
+import unittest
+from number_of_inversions import compute_inversions, compute_inversions_naive
+from random import randint
+
+
+class TestNumberOfInversions(unittest.TestCase):
+    def test_small(self):
+        for array in [
+            ([1, 2, 3]),
+            ([3, 2, 1]),
+            ([7, 2, 5, 3, 7, 13, 1, 6]) # WRONG ANSWER = 6. TRUE ANS NAIVE = 13
+        ]:
+            self.assertEqual(compute_inversions(array)[1],
+                             compute_inversions_naive(array))
+
+    def test_random(self):
+        for n in (10, 100):
+            for max_value in (1, 2, 10, 10 ** 5):
+                array = [randint(0, max_value) for _ in range(n)]
+                print("random_arr = ", array)
+                self.assertEqual(compute_inversions(array)[1],
+                                 compute_inversions_naive(array))
+
+    def test_large(self):
+        self.assertEqual(compute_inversions([1] * 100)[1], 0)
+        self.assertEqual(compute_inversions([2] * 100)[1], 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
